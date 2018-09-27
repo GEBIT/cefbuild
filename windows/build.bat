61fca1530ec7be0553d544bd795daa8c40650693
Rene Schneider <rene.schneider@gebit.de>
2018-09-27 09:31:56 +0200

    Changed Windows symbol level to 1
    On Windows, it is necessary to have symbols produced by the build because post-build
    scripts appear to fail if there are no symbol files. As on Windows, symbols are not
    part of the DLL but separate files, creating symbols does not increase DLL size and
    thus is not a problem at all, so we can easily have them generated there.

--- windows/build.bat
+++ windows/build.bat
@@ -1,6 +1,6 @@
 set /p BRANCH=<..\branch.txt
 
-set "GN_DEFINES=use_allocator=none is_official_build=true use_sysroot=true symbol_level=0"
+set "GN_DEFINES=use_allocator=none is_official_build=true use_sysroot=true symbol_level=1"
 set GYP_MSVS_VERSION=2017
 python automate-git.py --no-debug-build --minimal-distrib --client-distrib --force-clean --x64-build --force-build --branch=%BRANCH% --download-dir=c:\CEF\chromium_git --depot-tools-dir=c:\CEF\depot_tools
 
