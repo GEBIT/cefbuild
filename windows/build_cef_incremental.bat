a424587631cdf0a9052911a9664082cb9d22dca0
Rene Schneider <slartie@posteo.de>
2019-02-06 14:58:36 +0100

    added pause at end of windows bat files

--- windows/build_cef_incremental.bat
+++ windows/build_cef_incremental.bat
@@ -4,3 +4,4 @@ set "GN_DEFINES=use_allocator=none is_official_build=true use_sysroot=true symbo
 set GYP_MSVS_VERSION=2017
 python automate-git.py --no-debug-build --minimal-distrib --client-distrib --x64-build --force-build --branch=%BRANCH% --download-dir=c:\CEF\chromium_git --depot-tools-dir=c:\CEF\depot_tools
 
+pause
--- windows/update_cef.bat
+++ windows/update_cef.bat
@@ -4,3 +4,4 @@ set "GN_DEFINES=use_allocator=none is_official_build=true use_sysroot=true symbo
 set GYP_MSVS_VERSION=2017
 python automate-git.py --download-dir=c:\CEF\chromium_git --depot-tools-dir=c:\CEF\depot_tools --no-distrib --no-build --x64-build --branch=%BRANCH%
 
+pause
