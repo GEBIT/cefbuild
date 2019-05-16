4ea67ebc028bc19e97a472ec577fd56eeac06b31
Rene Schneider <rene.schneider@gebit.de>
2019-05-16 12:52:23 +0200

    removed checked-in automate-git.py, auto-downloading it now

--- linux/1_build_cef.sh
+++ linux/1_build_cef.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 
 read -r BRANCH<../branch.txt
 export GN_DEFINES="use_allocator=none is_official_build=true use_sysroot=true symbol_level=0 proprietary_codecs=true ffmpeg_branding=Chrome"
+
+echo "Downloading automate-git.py script from CEF repository"
+# Downloading the file from the master branch is the way to go according to the CEF build documentation, even if not building master, but a maintenance branch
+curl -o automate-git.py https://bitbucket.org/chromiumembedded/cef/raw/master/tools/automate/automate-git.py
+
 python automate-git.py --no-debug-build --minimal-distrib --client-distrib --force-clean --x64-build --build-target=cefsimple --force-build --branch=$BRANCH --download-dir=./../../chromium_git --depot-tools-dir=./../../depot_tools
 
--- windows/1_build_cef.sh
+++ windows/1_build_cef.sh
@@ -7,4 +7,8 @@ read -r BRANCH<../branch.txt
 export GN_DEFINES="use_allocator=none is_official_build=true use_sysroot=true symbol_level=1 proprietary_codecs=true ffmpeg_branding=Chrome"
 export GYP_MSVS_VERSION=2017
 
-python automate-git.py --no-debug-build --minimal-distrib --client-distrib --force-clean --x64-build --force-build --branch=$BRANCH --download-dir=./../../chromium_git --depot-tools-dir=./../../depot_tools
+echo "Downloading automate-git.py script from CEF repository"
+# Downloading the file from the master branch is the way to go according to the CEF build documentation, even if not building master, but a maintenance branch
+curl -o automate-git.py https://bitbucket.org/chromiumembedded/cef/raw/master/tools/automate/automate-git.py
+
+python automate-git.py --no-debug-build --minimal-distrib --client-distrib --force-clean --x64-build --force-build --build-target=cefsimple --branch=$BRANCH --download-dir=./../../chromium_git --depot-tools-dir=./../../depot_tools
