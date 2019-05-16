c51eff3b8391759f43adb9d56231ee97da2069b0
Rene Schneider <rene.schneider@gebit.de>
2019-05-16 18:27:28 +0200

    fixed wrong CEF repository

--- linux/1_build_cef.sh
+++ linux/1_build_cef.sh
@@ -7,6 +7,8 @@ export GN_DEFINES="use_allocator=none is_official_build=true use_sysroot=true sy
 echo "Downloading automate-git.py script from CEF repository"
 # Downloading the file from the master branch is the way to go according to the CEF build documentation, even if not building master, but a maintenance branch
 curl -o automate-git.py https://bitbucket.org/chromiumembedded/cef/raw/master/tools/automate/automate-git.py
+# But since the CEF repository URL is part of the script, we must replace that dynamically
+sed -i "s/cef_git_url = .*/cef_git_url = 'https:\/\/github.com\/GEBIT\/cef.git'/" automate-git.py
 
 python automate-git.py --no-debug-build --minimal-distrib --client-distrib --force-clean --x64-build --build-target=cefsimple --force-build --branch=$BRANCH --download-dir=./../../chromium_git --depot-tools-dir=./../../depot_tools
 
--- windows/1_build_cef.sh
+++ windows/1_build_cef.sh
@@ -10,5 +10,7 @@ export GYP_MSVS_VERSION=2017
 echo "Downloading automate-git.py script from CEF repository"
 # Downloading the file from the master branch is the way to go according to the CEF build documentation, even if not building master, but a maintenance branch
 curl -o automate-git.py https://bitbucket.org/chromiumembedded/cef/raw/master/tools/automate/automate-git.py
+# But since the CEF repository URL is part of the script, we must replace that dynamically
+sed -i "s/cef_git_url = .*/cef_git_url = 'https:\/\/github.com\/GEBIT\/cef.git'/" automate-git.py
 
 python automate-git.py --no-debug-build --minimal-distrib --client-distrib --force-clean --x64-build --force-build --build-target=cefsimple --branch=$BRANCH --download-dir=./../../chromium_git --depot-tools-dir=./../../depot_tools
