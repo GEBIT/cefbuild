5444e6d029aa86064436b4b4eb3b5b62339c6f8e
Rene Schneider <rene.schneider@gebit.de>
2019-10-25 18:26:19 +0200

    removed incremental build scripts
    These are not necessary anymore, since the 1_ scripts now accept the "incremental" parameter which does the same.

--- linux/build_cef_incremental.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-read -r BRANCH<../branch.txt
-export GN_DEFINES="use_allocator=none is_official_build=true use_sysroot=true symbol_level=0"
-python automate-git.py --no-update --no-debug-build --minimal-distrib --client-distrib --x64-build --build-target=cefsimple --force-build --branch=$BRANCH --download-dir=/home/cefbuild/code/chromium_git --depot-tools-dir=/home/cefbuild/code/depot_tools
-
