c15b191eece118bb51f9df409b6fe2f1dd4c29f4
Rene Schneider <rene.schneider@gebit.de>
2020-01-10 17:40:23 +0100

    added auto-cloning of JCEF repo if not found
    added auto-cloning of JCEF repo if not found
    
    
    added auto-cloning of JCEF repo if not found

--- windows/2_update_jcef.sh
+++ windows/2_update_jcef.sh
@@ -8,6 +8,10 @@ read -r BRANCH<../branch.txt
 
 JCEF_DIR=$BASEDIR/java-cef
 
+if [[ ! -d $JCEF_DIR ]]; then
+    git clone https://github.com/GEBIT/java-cef.git $JCEF_DIR
+fi
+
 echo "Checking out JCEF branch gebit_$BRANCH"
 git -C $JCEF_DIR fetch
 git -C $JCEF_DIR -c advice.detachedHead=false checkout -f origin/gebit_$BRANCH
