d3c8b28ffed3249c52398d092695cd8ca550eece
Rene Schneider <rene.schneider@gebit.de>
2021-01-10 22:58:33 +0100

    Added arm64 support to MacOS deployment script

--- macos/4_deploy_binaries.sh
+++ macos/4_deploy_binaries.sh
@@ -6,14 +6,25 @@ OUTPUT_DIR=./out
 
 read -r BRANCH<../branch.txt
 
-CEF_RELEASE_DIR=`find $OUTPUT_DIR -type d -name "cef_binary_*.$BRANCH.*_macosx64"`
+CEF_RELEASE_DIR_X64=`find $OUTPUT_DIR -type d -name "cef_binary_*.$BRANCH.*_macosx64"`
+CEF_RELEASE_DIR_ARM64=`find $OUTPUT_DIR -type d -name "cef_binary_*.$BRANCH.*_macosarm64"`
 
-if [ ! -d "$CEF_RELEASE_DIR" ]; then
-    echo "ERROR: Did not find a matching CEF branch release build in out directory"
-    exit
+if [ ! -d "$CEF_RELEASE_DIR_X64" ]; then
+    if [ ! -d "$CEF_RELEASE_DIR_ARM64" ]; then
+        echo "ERROR: Did not find a matching CEF branch release build in binary_distrib directory"
+        exit
+    else
+        echo "Found arm64 CEF build"
+        ARCH_NAME="arm64"
+        CEF_RELEASE_DIR=$CEF_RELEASE_DIR_ARM64
+    fi
+else
+    echo "Found x64 CEF build"
+    ARCH_NAME="x64"
+    CEF_RELEASE_DIR=$CEF_RELEASE_DIR_X64
 fi
 
-if [[ $CEF_RELEASE_DIR =~ cef_binary_(.+)_macosx64 ]]; then
+if [[ $CEF_RELEASE_DIR =~ cef_binary_(.+)_macos ]]; then
     CEF_RELEASE_VERSION=${BASH_REMATCH[1]}
 else
     echo "ERROR: Failed to extract CEF version number"
@@ -33,7 +44,7 @@ if [ ! -f "$JCEF_VERSION_FILE" ]; then
 fi
 
 # Really big binary is a safe indication of a debug build
-if [ $(stat -c%s "$OUTPUT_DIR/jcef-binaries-macos/jcef/libcef.dylib") -gt 1000000000 ]; then
+if [ $(stat -f%z "$OUTPUT_DIR/jcef-binaries-macos/Chromium Embedded Framework.framework/Chromium Embedded Framework") -gt 1000000000 ]; then
     BUILD_TYPE="Debug"
 else
     BUILD_TYPE="Release"
@@ -43,7 +54,7 @@ JCEF_RELEASE_VERSION=$(cat $JCEF_VERSION_FILE)
 
 echo "Found CEF version $CEF_RELEASE_VERSION (cleaned-up: $CEF_CLEAN_VERSION)"
 echo "Found JCEF version $JCEF_RELEASE_VERSION which will be our artifact version"
-echo "The build to be packaged is a $BUILD_TYPE build"
+echo "The build to be packaged is a $BUILD_TYPE build for $ARCH_NAME"
 
 if [ ! -f "$OUTPUT_DIR/jcef-binaries-macos.jar" ]; then
     echo "ERROR: Did not find jcef-binaries-macos.jar"
@@ -63,5 +74,5 @@ fi
 
 VERSION=$JCEF_RELEASE_VERSION-$QUALIFIER
 
-echo "Deploying JCEF binary package for MacOS in version $VERSION to Nexus"
-mvn -gs ../../mvn-settings/settings.xml deploy:deploy-file -DartifactId=jcef-binaries-macos -Dfile=$OUTPUT_DIR/jcef-binaries-macos.jar -Dversion=$VERSION
+echo "Deploying JCEF binary package for MacOS $ARCH_NAME in version $VERSION to Nexus"
+mvn -gs ../../mvn-settings/settings.xml deploy:deploy-file -DartifactId=jcef-binaries-macos-$ARCH_NAME -Dfile=$OUTPUT_DIR/jcef-binaries-macos.jar -Dversion=$VERSION
